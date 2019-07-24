42b6b2b9a7dc1cded688bacb9638719a15c08645
Aurelie <aurelie.benovici@gmail.com>
2019-07-24 11:15:57 +0200

    start pagination

--- components/Data.js
+++ components/Data.js
@@ -1,32 +1,40 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { ScrapeContext } from "./ScrapeContext";
-import { getNewPost } from "../apis";
+// import { getNewPost } from "../apis";
+
+import Pagination from "./Paginations.js";
 
 export default function Data() {
   const scrapes = useContext(ScrapeContext);
+  // const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage] = useState(10);
   console.log("hello data data.js", scrapes.scrapes);
 
-  // const handleSubmit = () => {
-  //   console.log("click is press");
-  //   getNewPost().then(response => {
-  //     console.log("Go to scrape :", response.data);
-  //     setScrappe(...response);
-  //   });
-  // };
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  console.log(indexOfFirstPost, "last", indexOfLastPost);
+
+  //  Get current post
+  const currentPosts =
+    scrapes.scrapes.length > 0
+      ? scrapes.scrapes.slice(
+          indexOfFirstPost,
+          indexOfLastPost
+        )
+      : "pas de donner";
+  console.log(currentPosts);
+
+  //  Get current page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
     <div>
-      <h2>Your Data</h2>
-      {/* <p>
-        {" "}
-        What's new ?
-        <button onClick={handleSubmit()}>
-          Get new post
-        </button>{" "}
-      </p> */}
+      <h2>Your Data :</h2>
 
       {scrapes.scrapes.length > 0 ? (
         <div>
-          {scrapes.scrapes.map((item, i) => {
+          {currentPosts.map((item, i) => {
             return (
               <div key={i}>
                 <h2>{item.name}</h2>
@@ -42,6 +50,12 @@ export default function Data() {
           <p>Pas de donnée</p>
         </div>
       )}
+
+      <Pagination
+        postsPerPage={postPerPage}
+        totalPosts={scrapes.scrapes.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
